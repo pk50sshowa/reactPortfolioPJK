6567e6c1523941518ff0a8dd0fcaf660dbdc5c88
Paul Koehler <paul.koehler@gmail.com>
2023-08-04 23:33:31 -0700

    About page updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
             <h1>Home</h1>
             <p>Welcome to Paul Koehler's portfolio page! Please take a look at the various projects on this page, and feel free to contact Paul directly at <a href="mailto:paul.koehler@gmail.com">paul.koehler@gmail.com</a>.</p>
             <p>
-            I am seeking employment as a software developer using experience as a Japanese-English translator and intepreter. My portfolio on this page shows past projects I have worked on, and you can also follow my <a href="http://www.github.com/pk50sshowa/" target="_blank" rel="noopener noreferrer">GitHub account</a>.
+            Paul Koehler is a full stack developer with a passion for learning new technologies and in particular making sure server interfaces communicate with an entire application. He comes into this with an extensive background as a Japanese to English translator and interpreter. My portfolio on this page shows past projects I have worked on, and you can also follow my <a href="http://www.github.com/pk50sshowa/" target="_blank" rel="noopener noreferrer">GitHub account</a>.
             </p>
             <p>
             初めまして。ポールと申します。日英翻訳・通訳者の経験を生かして、ソフトウェア開発の仕事を探しております。よろしくお願いします。
